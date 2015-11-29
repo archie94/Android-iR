a1a06c2a349e939ff79536b7eb4d5d6f753a912d
Arka Prava Basu <arkaprava94@gmail.com>
2015-11-30 02:15:24 +0530

    Image resize action complete

--- src/com/prizm/imagerefractor/ImageEditor.java
+++ src/com/prizm/imagerefractor/ImageEditor.java
@@ -1,12 +1,16 @@
 package com.prizm.imagerefractor;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -20,6 +24,8 @@ public class ImageEditor extends Activity
 	EditText width,height;
 	Button resize;
 	int position;
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
@@ -36,6 +42,39 @@ public class ImageEditor extends Activity
 		resize= (Button)findViewById(R.id.image_editor_button_resize);
 		
 		Toast.makeText(this, ""+myImages.size(), Toast.LENGTH_SHORT).show();
+		resize.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View arg0) 
+			{
+				// TODO Auto-generated method stub
+				if(Integer.parseInt(width.getText().toString())>0 && Integer.parseInt(height.getText().toString())>0)
+				{
+					Bitmap myBitmap = BitmapFactory.decodeFile(myImages.get(position).getAbsolutePath());
+					myBitmap = Bitmap.createScaledBitmap(myBitmap, Integer.parseInt(width.getText().toString()), Integer.parseInt(height.getText().toString()), true);
+					File myDir = new File(Environment.getExternalStorageDirectory().toString()+"/IR_Saved_Images");
+					myDir.mkdirs();
+					File newImg = new File(myDir ,myImages.get(position).getName().replace(".jpg", "").replace(".jpeg", "").replace(".png", "")+"_resize.jpeg" );
+					if(newImg.exists())
+						newImg.delete();
+					try
+					{
+						FileOutputStream outputFile = new FileOutputStream(newImg);
+						myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, outputFile);
+						outputFile.flush();
+						outputFile.close();
+						
+					}
+					catch(Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+			
+		});
+		
 	}
 
 	
@@ -50,15 +89,6 @@ public class ImageEditor extends Activity
 			myBitmap = Bitmap.createScaledBitmap(myBitmap, imageView.getWidth(), imageView.getHeight(), true);
 			imageView.setImageBitmap(myBitmap);
 		}
-	}
-
-
-	@Override
-	protected void onPause() 
-	{
-		// TODO Auto-generated method stub
-		super.onPause();
-		finish();
 	}
 	
 	
